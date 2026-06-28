9c31219c3ccc07ab0d931f5b6ca1c22a1b981ead
Bryan Ebagua <bryanebagua@gmail.com>
2026-06-27 22:03:40 -0400

    wrote load_tasks and save_tasks

--- storage.py
+++ storage.py
@@ -1,6 +1,16 @@
+from pathlib import Path
+import json
 #Checks if tasks.json exists. Returns a parsed json list if it does, or an empty list if not
 def load_tasks():
-
+    file_path = Path("tasks.json")
+    if file_path.exists():
+        with open("tasks.json", "r") as file:
+            tasks = json.load(file)
+            return tasks
+    else:
+        return[]
 # Takes a list of tasks and writes them to the json file
 def save_tasks(tasks):
-    
+    with open("tasks.json", "w") as file:
+        json.dump(tasks, file)
+    return
