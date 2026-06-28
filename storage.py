b47ecda0ec50d388279a800055ab0f0d152b51ae
Bryan Ebagua <bryanebagua@gmail.com>
2026-06-27 22:07:10 -0400

    edited code for readability

--- storage.py
+++ storage.py
@@ -1,16 +1,17 @@
 from pathlib import Path
 import json
+file_name = "tasks.json"
 #Checks if tasks.json exists. Returns a parsed json list if it does, or an empty list if not
 def load_tasks():
-    file_path = Path("tasks.json")
+    file_path = Path(file_name)
     if file_path.exists():
-        with open("tasks.json", "r") as file:
+        with open(file_path, "r") as file:
             tasks = json.load(file)
             return tasks
     else:
-        return[]
+        return []
 # Takes a list of tasks and writes them to the json file
 def save_tasks(tasks):
-    with open("tasks.json", "w") as file:
-        json.dump(tasks, file)
+    with open(file_name, "w") as file:
+        json.dump(tasks, file, indent=4)
     return
