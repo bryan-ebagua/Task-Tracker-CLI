d5706326b5bb8be1a4a67d147f9a38c4c52b24d1
Bryan Ebagua <bryanebagua@gmail.com>
2026-06-27 21:49:39 -0400

    created key files

---
+++ storage.py
@@ -0,0 +1,6 @@
+#Checks if tasks.json exists. Returns a parsed json list if it does, or an empty list if not
+def load_tasks():
+
+# Takes a list of tasks and writes them to the json file
+def save_tasks(tasks):
+    
